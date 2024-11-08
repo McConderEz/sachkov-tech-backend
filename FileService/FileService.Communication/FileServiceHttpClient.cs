05688df1c08d4ef5672028b8819f79bedbf3b601
kirill_sachkov <sachkovkrl@gmail.com>
2024-11-08 20:17:00 +0300

    commit

---
+++ FileService/FileService.Communication/FileServiceHttpClient.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http.Json;
+using FileService.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace FileService.Communication;
+
+public class FileServiceClient(HttpClient httpClient)
+{
+    public async Task<IReadOnlyList<FileResponse>> GetFilesByIdsAsync(Guid[] ids, CancellationToken cancellationToken)
+    {
+        var request = new GetFilesByIdsRequest(ids);
+
+        var response = await httpClient.PostAsJsonAsync("files", request, cancellationToken);
+
+        response.EnsureSuccessStatusCode();
+
+        var files = await response.Content.ReadFromJsonAsync<IEnumerable<FileResponse>>(cancellationToken);
+
+        return files?.ToList() ?? [];
+    }
+}
+
+public static class HttpServiceStartup
+{
+    public static IServiceCollection AddFileServiceClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<FileServiceOptions>(configuration.GetSection(""));
+        
+        services.AddHttpClient<FileServiceClient>((serviceProvider, httpCffgtlient) =>
+        {
+            var fileServiceOptions = serviceProvider.GetRequiredService<IOptions<FileServiceOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(fileServiceOptions.HttpAdress);
+        });
+    }
+}
+
+public class FileServiceOptions
+{
+    public string HttpAdress { get; set; }
+}
